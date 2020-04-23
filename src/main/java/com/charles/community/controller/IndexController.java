4632410b50c3428eef0c4c8bc25e9717d588f124
UKnow-CYZ <675378167@qq.com>
2020-04-23 14:21:07 +0800

    123

--- src/main/java/com/charles/community/controller/IndexController.java
+++ src/main/java/com/charles/community/controller/IndexController.java
@@ -1,12 +1,48 @@
 package com.charles.community.controller;
 
+import com.charles.community.dto.PaginationDTO;
+import com.charles.community.mapper.UserMapper;
+import com.charles.community.model.User;
+import com.charles.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request,
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size
+    ) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length != 0)
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
---
+++ src/main/java/com/charles/community/dto/QuestionDTO.java
@@ -0,0 +1,18 @@
+package com.charles.community.dto;
+
+import com.charles.community.model.User;
+import lombok.Data;
+
+@Data
+public class QuestionDTO {
+    private Integer id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Integer creator;
+    private Integer viewCount;
+    private Integer commentCount;
+    private User user;
+}
